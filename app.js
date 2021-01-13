a9b1eca9eb94b74219a45376faed5de36915078d
Steve <S_Reich.1@web.de>
2021-01-13 22:00:40 +0100

    added mqtt
    mqtt connection to TTN established wit mqtt.js

--- app.js
+++ app.js
@@ -2,10 +2,42 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const mqtt = require('mqtt');
 require('dotenv/config');
+
+
+
+
+//mqtt
+
+//var client  = mqtt.connect("mqtt://test.mosquitto.org",{clientId:"soundsensetest"});
+//client.on("connect",function(){	
+//console.log("connected to mqtt");
+//});
+
+
+ var client = mqtt.connect("mqtt://eu.thethings.network:1883",{ 
+ clientId:"soundsenstest1",
+ username:"lora_soundsense",
+ password:"TTN_KEY",
+ clean:true
+ });
+
+ client.on("connect", function(){ 
+ console.log("connected to mqtt");
+ });
+
+
+
+
+
+
+
 
 //MIDDLEWARE
 
+app.use(cors());
 app.use(bodyParser.json());
 
 //IMPORT ROUTES
--- routes/posts.js
+++ routes/posts.js
@@ -3,15 +3,23 @@ const router = express.Router();
 const Post = require('../models/post');
 
 
-router.get('/', (req, res) => {
-  res.send('We are on posts');
+
+router.get('/', async (req, res) => {
+  try{
+      const posts = await Post.find();
+      res.json(posts);
+  }catch (err) {
+    res.json({message: err});
+
+  }
+  
 });
 
 //router.get('/specific', (req,res) => {
 //res.send('specific posts');
 //});
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   //console.log(req.body);
   const post = new Post({
     title: req.body.title,
@@ -19,19 +27,13 @@ router.post('/', (req, res) => {
 
   });
   //console.log(post)
-  post.save()
-    .then(data => {
-      console.log(data)
-      res.status(200).json(data);
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({
-        message: err
-      });
-    
-    });
-
+  
+  try{
+  const savedPost = await post.save()
+  res.json(savedPost);
+  }catch(err){
+      res.json({message: err})
+  }
 
 });
 
